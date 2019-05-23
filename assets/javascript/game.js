d2bfb7f845cb2392d7e42e9b006c2b339ebc5218
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-05-22 19:42:56 -0700

    resetElements method to iteratively create new word setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,25 @@
 var wins = 0;
-var words = [{ w: "brian chesky", h: "Airbnb" }, { w: "patrick collison", h: "Stripe" }, { w: "elon musk", h: "SpaceX" }, 
-{ w: "stewart butterfield", h: "Slack" }, {w: "max mullen", h: "Instacart"}, {w: "baiju bhatt", h: "Robinhood"}, 
-{w: "charlie cheever", h: "quora"}, {w: "neil blumenthal", h: "Warby Parker"}, {w: "adam neumann", h: "WeWork"},
+var words = [{ w: "chesky", h: "Airbnb" }, { w: "collison", h: "Stripe" }, { w: "musk", h: "SpaceX" },
+{ w: "butterfield", h: "Slack" }, { w: "mullen", h: "Instacart" }, { w: "bhatt", h: "Robinhood" },
+{ w: "cheever", h: "quora" }, { w: "blumenthal", h: "Warby Parker" }, { w: "neumann", h: "WeWork" },
 ];
 var guessedLetters = [];
 var numWords = 0;
-var currentWord
-document.getElementById("wins").textContent = "Wins: " + wins;
+var currentWord;
+var remainingGuesses;
+function resetElements() {
+    var index = Math.floor(Math.random() * words.length);
+    console.log(index);
+    var currentWord = words[index].w;
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    var placeholder = "";
+    for (var i = 0; i < currentWord.length; i++) {
+        placeholder = placeholder + "_ ";
+    }
+    document.getElementById("word").textContent = "Word: " + placeholder;
+    document.getElementById("founder").textContent = "Founder of " + words[index].h;
+    console.log(currentWord);
+    guessedLetters = [];
+}
+
+resetElements();
