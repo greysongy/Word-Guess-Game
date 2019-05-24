35c7bd77e9eb6a1bd733ffbcda7677708998868c
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-05-23 20:17:47 -0700

    commented code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// delcations of global variables that will be referenced throughout
 var wins = 0;
 var words = [{ w: "chesky", h: "Airbnb" }, { w: "collison", h: "Stripe" }, { w: "musk", h: "SpaceX" },
 { w: "butterfield", h: "Slack" }, { w: "mullen", h: "Instacart" }, { w: "bhatt", h: "Robinhood" },
@@ -13,7 +14,10 @@ var outOfWords = false;
 var currentIndex;
 var currentWord;
 var placeholder;
+
+// generic function that iteratively resets elements after a user runs out of guesses/correctly chooses a word
 function resetElements() {
+    //conditoinal to ensure this does't happen once the game runs out of words
     if (!gameOver) {
         currentIndex = Math.floor(Math.random() * words.length);
         currentWord = words[currentIndex].w;
@@ -22,6 +26,7 @@ function resetElements() {
         for (var i = 0; i < currentWord.length; i++) {
             placeholder = placeholder + "_ ";
         }
+        //updates the visual text to be consistent with the current values
         document.getElementById("word").textContent = "Word: " + placeholder;
         document.getElementById("founder").textContent = "Founder of " + words[currentIndex].h;
         guessedLetters = [];
@@ -31,8 +36,10 @@ function resetElements() {
     }
 }
 
+//helper function designed to change one char of a string, since we are only allowed to access characters of a string using an index
 function setCharAt(str, index, chr) {
     var temp = "";
+    //sets new values of all temp characters, before ultimately returning that string
     for (var i = 0; i < str.length; i++) {
         if (i === index) {
             temp += chr;
@@ -44,6 +51,7 @@ function setCharAt(str, index, chr) {
     return temp;
 }
 
+//helper function that checks if a user has correctly guessed a word (by looking to see if there are any empty slots remaining)
 function checkWin() {
     for (var i = 0; i < placeholder.length - 1; i += 2) {
         if (placeholder[i] === "_") {
@@ -58,14 +66,17 @@ function checkWin() {
 
 resetElements();
 
+//main body that reacts to key presses by the user
 document.onkeyup = function (event) {
     var currentGameOver = false;
     if (!gameOver) {
         var key = event.key;
+        //conditionals that check that a character is a valid key, , not in guessed letters, is in the word
         if (key.match((/[a-z]/i))) {
             if (!guessedLetters.includes(key)) {
                 if (currentWord.includes(key)) {
                     for (var i = 0; i < currentWord.length; i++) {
+                        //cody of work
                         if (currentWord[i] === key) {
                             placeholder = setCharAt(placeholder, (2 * i), key);
                             document.getElementById("word").textContent = "Word: " + placeholder;
@@ -73,6 +84,7 @@ document.onkeyup = function (event) {
                     }
                 }
                 else {
+                    //alternative that decreases remaining guesses by one, and checks if you're out of guesses
                     remainingGuesses--;
                     document.getElementById("guesses").textContent = "Remaining Guesses: " + remainingGuesses;
                     if (remainingGuesses === 0) {
@@ -94,10 +106,10 @@ document.onkeyup = function (event) {
                 document.getElementById("letters").textContent = "Guessed Letters: " + newString;
 
             }
+            //final check to see if you've won, and if the entire game is over (we've run out of words) 
             if (checkWin()) {
                 words.splice(currentIndex, 1);
                 numWords++;
-                console.log("Number of Words " + numWords);
                 if (numWords === 10) {
                     gameOver= true;
                     alert("Game Over");
